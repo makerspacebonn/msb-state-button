886b12877b21415e500293675d750d5b31f336a4
Falk Kühnel <falk@kuehnel.eu>
2026-01-20 23:56:16 +0100

    docs: add comment explaining API key purpose

--- src/secrets_example.py
+++ src/secrets_example.py
@@ -2,6 +2,7 @@ mqtt_server = 'mqtt.example.com'
 mqtt_user = 'username'
 mqtt_pass = 'password'
 
+# API key for authenticating state changes to status.makerspacebonn.de
 API_key = 'your-api-key'
 
 # Dictionary of known WiFi networks to connect to
